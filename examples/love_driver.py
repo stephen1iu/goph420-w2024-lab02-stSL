1d1739a95c731f742df6f4f31dfd4ff9c370ba5c
stephen1iu <144289910+stephen1iu@users.noreply.github.com>
2024-03-24 12:57:19 -0600

    Update love_driver.py

--- examples/love_driver.py
+++ examples/love_driver.py
@@ -9,16 +9,7 @@ def main():
     B2=3200
     H=4000
 
-    """def f(x,f):
-        return (np.tan(2*np.pi*f*x)*x*(p1/p2))**2+x**2-H**2*(B1**-2-B2**-2)
-    def f2(x):
-        return (np.tan(2*np.pi*2*x)*x*(p1/p2))**2+x**2-H**2*(B1**-2-B2**-2)
-    def feq2(x):
-        return H*np.sqrt(B1**-2-x**-2)
-    def dfdx1(x):
-        return 2*x*(np.tan(2*np.pi*x)**2+2*np.pi*x*np.arcsin(2*np.pi*x)**2*np.tan(2*np.pi*x))+1
-    def dfdx2(x):
-        return"""
+    f=[0.1, 0.3, 0.5, 0.7, 0.9]
     
     def g(x,f):
         return (p1/p2)*(np.sqrt(H**2*(B1**-2-B2**-2)-x**2)/x)-np.tan(2*np.pi*f*x)
@@ -27,10 +18,9 @@ def main():
         return -(np.sqrt((B1**-2-B2**-2)*H**2-x**2)/x**2)-(1/np.sqrt((B1**-2-B2**-2)*H**2-x**2))-(2*np.pi*f*np.arccos(2*np.pi*f*x)**2)
     
     for f in range (0,6):
-        g(x,f)
-        dgdx(x,f)
+        g(x, f)
+        dgdx(x ,f)
     
-
 
 if __name__=="__main__":
     main()
