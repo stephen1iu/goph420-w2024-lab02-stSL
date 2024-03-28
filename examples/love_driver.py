c93d3b8fcf68a93eae074aa819a275231e60c74d
Stephen Liu <stephen.liu@ucalgary.ca>
2024-03-27 21:38:23 -0600

    driver not working, have no idea whats wrong yaaaay

--- examples/love_driver.py
+++ examples/love_driver.py
@@ -11,33 +11,35 @@ def main():
 
     cl=[]
     wavelength=[]
-    frequencies=[0.01, 0.1, 0.5, 1, 2, 5]
+    frequencies=[0.01, 0.1, 0.5, 1, 5, 10]
+    zeta_arr=[]
 
-    zeta_max=np.sqrt(H**2*(B1**-2-B2**-2))
-
-    #find zeta max
-    #based on asymptotes
-    #zeta**2 cant be > H^2*(B1^-2-B2^-2) 
-    #tan(2 pi f)zeta has asymptotes
-    #2k+1 for asymptotes    2k+1(pi/2)=tan(2 pi f)zeta
-    #find asymptotes between 0 and zeta max
-    #while zeta_a < zeta_max
-    #always use primary mode, mode closest to 0
-    #use small offset of asymptote for initial guess (guess=asymptote-1e-5)
-    k=0
-    while zeta_a < zeta_max:
-        zeta_a=((2*k+1)*(np.pi/2))/np.tan(2*np.pi*f)
-        k+=1
+    zeta_max=np.sqrt(H**2 * (B1**-2 - B2**-2)) #1.6939991038925966
 
     for f in frequencies:
-        def g(x,f):
-            return (p1/p2)*(np.sqrt(H**2*(B1**-2-B2**-2)-x**2)/x)-np.tan(2*np.pi*f*x)
-        def dgdx(x,f):
-            return -(np.sqrt((B1**-2-B2**-2)*H**2-x**2)/x**2)-(1/np.sqrt((B1**-2-B2**-2)*H**2-x**2))-(2*np.pi*f*np.arccos(2*np.pi*f*x)**2)
+        k=0
+        x0=(2*k+1) / (4*f)
+        zeta_a=[]
+
+        def g(x):
+            return (p2/p1)*(np.sqrt(H**2*(zeta_max**2-x**2))/x)-np.tan(2*np.pi*f*x)
+        def dgdx(x):
+            return -((p2*(zeta_max**2))/(p1*(x**2)*np.sqrt((zeta_max**2-x**2)))) - (2*np.pi*f*((1/np.cos(2*np.pi*f*x))**2))
+        print (f"freq {f}")
         
-        x, _, eps_a = root_newton_raphson(x, g, dgdx)
-        cl.append(1/np.sqrt(B1**-2-(x/H)**2))
-        wavelength.append(cl/f)
+        while x0 < zeta_max:
+            zeta_a.append(x0)
+            k+=1
+            x0=(2*k+1) / (4*f) - 1e-5
+
+        print (zeta_a)
+
+        for zeta in zeta_a:
+            zeta_0 = zeta
+            x, _, eps_a = root_newton_raphson(zeta_0, g, dgdx)
+            zeta_arr.append(x)
+        # cl.append(1/np.sqrt(B1**-2-(x/H)**2))
+        # wavelength.append(cl/f)
 
 if __name__=="__main__":
     main()
--- src/goph420_lab02/root.py
+++ src/goph420_lab02/root.py
@@ -22,13 +22,13 @@ def root_newton_raphson(x0, f, dfdx):
 
     while np.abs(eps_a[i])>tol:
         x_old=x
-        x=(x-(f(x)/dfdx(x)))
-        eps_a.append((x-x_old)/(x))
+        x=(x - (f(x) / dfdx(x)))
+        eps_a.append((x - x_old) / (x))
         i+=1
-        if i>150:
-            raise RuntimeWarning ("no root found")
+        if i>200:
+            raise RuntimeWarning ("Algorithm couldn't find a solution")
     return(
         x,
         i,
-        eps_a[1:] #doesnt return intializer value of eps_a
+        np.array(eps_a[1:]) #doesnt return intializer value of eps_a
     )
