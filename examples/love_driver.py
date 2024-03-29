c5e219a256b04bb95960f9b9921a9507d1173b80
Stephen Liu <stephen.liu@ucalgary.ca>
2024-03-28 20:21:24 -0600

    fixed some issues with driver, still need code for graphing

--- examples/love_driver.py
+++ examples/love_driver.py
@@ -13,33 +13,48 @@ def main():
     wavelength=[]
     frequencies=[0.01, 0.1, 0.5, 1, 5, 10]
     zeta_arr=[]
+    cl_arr=[]
+    wavelength_arr=[]
 
     zeta_max=np.sqrt(H**2 * (B1**-2 - B2**-2)) #1.6939991038925966
 
     for f in frequencies:
         k=0
-        x0=(2*k+1) / (4*f)
+        plt.figure()
+
         zeta_a=[]
+        zeta_arr_new = []
+        cl=[]
+        wavelength=[]
+        zeta_a_new = (2*k+1) / (4*f)
 
         def g(x):
-            return (p2/p1)*(np.sqrt(H**2*(zeta_max**2-x**2))/x)-np.tan(2*np.pi*f*x)
+            return (p2/p1)*np.sqrt(zeta_max**2-x**2)/x - np.tan(2*np.pi*f*x)
         def dgdx(x):
-            return -((p2*(zeta_max**2))/(p1*(x**2)*np.sqrt((zeta_max**2-x**2)))) - (2*np.pi*f*((1/np.cos(2*np.pi*f*x))**2))
-        print (f"freq {f}")
+            return -(p2/p1)/np.sqrt(zeta_max**2-x**2) - (p2/p1)*np.sqrt(zeta_max**2-x**2)/x**2 - 2*np.pi*f*((1/np.cos(2*np.pi*f*x))**2)
         
-        while x0 < zeta_max:
-            zeta_a.append(x0)
+        while zeta_a_new < zeta_max:
+            zeta_a.append(zeta_a_new)
             k+=1
-            x0=(2*k+1) / (4*f) - 1e-5
+            zeta_a_new = (2*k+1) / (4*f)
+        if g(zeta_max) < 0:
+            zeta_a.append(zeta_max)
 
-        print (zeta_a)
+        i=0
 
         for zeta in zeta_a:
-            zeta_0 = zeta
+            zeta_0 = zeta - 1e-5
             x, _, eps_a = root_newton_raphson(zeta_0, g, dgdx)
-            zeta_arr.append(x)
-        # cl.append(1/np.sqrt(B1**-2-(x/H)**2))
-        # wavelength.append(cl/f)
+            zeta_arr_new.append(x)
+            cl.append(1/np.sqrt(B1**-2-(x/H)**2))
+            wavelength.append(cl[i]/f)
+            i+=1
+        zeta_arr.append(zeta_arr_new)
+        cl_arr.append(cl)
+        wavelength_arr.append(wavelength)
+        
+        plt.plot(zeta_arr, [-10,10])
+        plt.savefig(f"frequency {f}")
 
 if __name__=="__main__":
     main()
